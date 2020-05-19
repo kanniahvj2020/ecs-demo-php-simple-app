3bbdd55b71df93aab01f51cd0dd52e5425617137
kanniahvj2020 <64630641+kanniahvj2020@users.noreply.github.com>
2020-05-19 14:58:50 +0530

    AWS ECS Demo

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>SA Launch Team3 - Tech Innovators</title>
+        <title>ECS Demo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="assets/css/bootstrap.min.css" rel="stylesheet">
         <style>body {margin-top: 40px; background-color: #333;}</style>
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>SA Launch Team3 - Tech Innovators</h1>
-                <h2>AWS CI/CD Demo App V2</h2>
+                <h1>AWS ECS Demo</h1>
+                <h2>DevOps Demo App V2</h2>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS via Fargate (Serverless).</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
